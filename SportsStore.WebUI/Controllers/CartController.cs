788ca794d6301b2183bc16c4720318efefe1f360
Piotrek <pioek22222@gmail.com>
2017-09-18 20:16:25 +0200

    change title layout

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -46,10 +46,12 @@ namespace SportsStore.WebUI.Controllers
             }
             return RedirectToAction("Index", new { returnUrl });
         }
+
         public PartialViewResult Summary(Cart cart)
         {
             return PartialView(cart);
         }
+
         public ViewResult Checkout()
         {
             return View(new ShippingDetails());
